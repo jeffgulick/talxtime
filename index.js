1d375b72cc90946066de96bb44c893a1423998c4
JEFFGULICK <jeffgulick@gmail.com>
2021-03-26 20:25:11 -0500

    test

--- index.js
+++ index.js
@@ -57,15 +57,15 @@ io.on("connection", socket => {
   });
 });
 
-// app.use(express.static(path.join(__dirname, 'client/build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+ '/client/build/index.html'))
-})
+app.use(express.static(path.join(__dirname, 'client/build')))
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+ '/client/build/index.html'))
+// })
 app.enable("trust proxy");
-app.use(express.static("client/build"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+// app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 
 
 const port = process.env.PORT || 3001;
