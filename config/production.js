36176e37ad96079446a073d875b524c5bc65d7ee
JEFFGULICK <jeffgulick@gmail.com>
2021-03-26 22:45:26 -0500

    still working

--- config/production.js
+++ config/production.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-    mongoURI: 
-        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@googlecloudfree.m2xpo.mongodb.net/ChatTime`
+    mongoURI:
+        'mongodb+srv://el_jefe0:hello@googlecloudfree.m2xpo.mongodb.net/ChatTime'
+
+        // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@googlecloudfree.m2xpo.mongodb.net/ChatTime`
 
 }
